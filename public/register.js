57d263df2a5e1be496b43cec787ca509f2505fc2
avisre <90497762+avisre@users.noreply.github.com>
2025-05-18 15:35:13 +0100

    Update register.js

--- public/register.js
+++ public/register.js
@@ -2,7 +2,7 @@
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
         try {
-          const response = await fetch("http://localhost:5000/api/register", {
+          const response = await fetch("https://fantasy-back-1.onrender.com/api/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email, password }),
